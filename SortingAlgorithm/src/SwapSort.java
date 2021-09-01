7777e34c933a6f349cce28305b499014429af578
ChaR <1215585066@qq.com>
2021-09-01 20:02:13 +0800

    feat：SwapSort.QuickSort

--- SortingAlgorithm/src/SwapSort.java
+++ SortingAlgorithm/src/SwapSort.java
@@ -5,12 +5,18 @@ public class SwapSort {
         //直接插入排序测试
         int[] a = {2,1,8,9,23,12,21,5};
 
-        int[] end = BubbleSort(a);
+        //int[] end = BubbleSort(a);
 
 
+//        for (int i = 0; i < end.length; ++i){
+//            System.out.print(end[i] + " ");
+//        }
 
-        for (int i = 0; i < end.length; ++i){
-            System.out.print(end[i] + " ");
+
+        QuickSort(a, 0 , a.length - 1);
+
+        for (int i = 0; i < a.length; ++i){
+            System.out.print(a[i] + " ");
         }
 
     }
@@ -46,5 +52,35 @@ public class SwapSort {
     }
 
 
+    public static void QuickSort(int[] a, int low, int high){
+
+        int temp; //基准数
+        int i = low, j = high;
+
+        if(low < high){
+            temp = a[low];
+            while (i < j) {
+
+                while (i < j && a[j] > temp) j--; //向左找比基准数小的，并交换
+                if (i < j) { //向左过程，a[i]的位置一直是基准数所空下来的
+                    a[i] = a[j];
+                    ++i;
+                }
+                while ((i < j && a[i] < temp)) i++;//向右找比基准数大的，并交换
+                if (i < j) {
+                    a[j] = a[i];
+                    --j;
+                }
+            }
+            a[i] = temp;
+
+            QuickSort(a, low, i-1);
+            QuickSort(a,i+1, high);
+
+        }
+
+
+
+    }
 
 }
